b7d31d575b5778ef7858a3fc2f829be34cd10956
Paweł Płatek <e2.8a.95@gmail.com>
2018-06-21 12:45:02 +0200

    done

---
+++ go_parser/CMakeFiles/go_parser.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "src/generated/Go2LLVMLexer.cpp"
+  "src/generated/Go2LLVMParser.cpp"
+  "src/generated/Go2LLVMBaseListener.cpp"
+  "src/generated/Go2LLVMBaseVisitor.cpp"
+  "src/generated/Go2LLVMListener.cpp"
+  "src/generated/Go2LLVMVisitor.cpp"
+  "CMakeFiles/go_parser.dir/src/go_parser.cpp.o"
+  "CMakeFiles/go_parser.dir/src/Helpers.cpp.o"
+  "CMakeFiles/go_parser.dir/src/visitors/Go2LLVMMyVisitor.cpp.o"
+  "CMakeFiles/go_parser.dir/src/exceptions/Go2LLVMError.cpp.o"
+  "CMakeFiles/go_parser.dir/src/exceptions/Go2LLVMWarning.cpp.o"
+  "CMakeFiles/go_parser.dir/src/visitors/VisitorTopLevel.cpp.o"
+  "CMakeFiles/go_parser.dir/src/visitors/VisitorStatement.cpp.o"
+  "CMakeFiles/go_parser.dir/src/visitors/VisitorDeclaration.cpp.o"
+  "CMakeFiles/go_parser.dir/src/visitors/VisitorExpression.cpp.o"
+  "CMakeFiles/go_parser.dir/src/visitors/VisitorOperand.cpp.o"
+  "CMakeFiles/go_parser.dir/src/visitors/VisitorFunction.cpp.o"
+  "CMakeFiles/go_parser.dir/src/variables/BasicVar.cpp.o"
+  "CMakeFiles/go_parser.dir/src/variables/IntegerVar.cpp.o"
+  "CMakeFiles/go_parser.dir/src/variables/FloatVar.cpp.o"
+  "CMakeFiles/go_parser.dir/src/variables/VarFactory.cpp.o"
+  "CMakeFiles/go_parser.dir/src/variables/PointerVar.cpp.o"
+  "CMakeFiles/go_parser.dir/src/variables/VoidVar.cpp.o"
+  "CMakeFiles/go_parser.dir/src/variables/BoolVar.cpp.o"
+  "CMakeFiles/go_parser.dir/src/generated/Go2LLVMLexer.cpp.o"
+  "CMakeFiles/go_parser.dir/src/generated/Go2LLVMParser.cpp.o"
+  "CMakeFiles/go_parser.dir/src/generated/Go2LLVMBaseListener.cpp.o"
+  "CMakeFiles/go_parser.dir/src/generated/Go2LLVMBaseVisitor.cpp.o"
+  "CMakeFiles/go_parser.dir/src/generated/Go2LLVMListener.cpp.o"
+  "CMakeFiles/go_parser.dir/src/generated/Go2LLVMVisitor.cpp.o"
+  "go_parser.pdb"
+  "go_parser"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/go_parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ go_parser/CMakeFiles/testing.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gros/studia/eaiib_3b/kompilatory/go_parser/testing/testing.cpp" "/home/gros/studia/eaiib_3b/kompilatory/go_parser/CMakeFiles/testing.dir/testing/testing.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "antlr_runtime_unix/include/antlr4-runtime"
+  "antlr_runtime_unix/include/antlr4-runtime/misc"
+  "antlr_runtime_unix/include/antlr4-runtime/atn"
+  "antlr_runtime_unix/include/antlr4-runtime/dfa"
+  "antlr_runtime_unix/include/antlr4-runtime/tree"
+  "antlr_runtime_unix/include/antlr4-runtime/support"
+  "src/generated"
+  "/usr/lib/llvm-3.8/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
